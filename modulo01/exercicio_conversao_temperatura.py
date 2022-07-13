88eac2b145d6c171a570b69ab00182b99320bf56
isaacbrds <isaac.brigido@gmail.com>
2022-07-13 20:13:38 -0300

    Python full - Aula13 exercicio conversão de temperatura

---
+++ modulo01/exercicio_conversao_temperatura.py
@@ -0,0 +1,5 @@
+temperatura_faren = float(input('Digite a temperatura em farenheit:  '))
+
+temperatura_celcius = 5 * (temperatura_faren - 32)/9
+
+print(f'A temperatura em celcius é: {temperatura_celcius}')
