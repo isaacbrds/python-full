3c6ef1a8c14ed7e09b1724e1a73dcb73576af207
isaacbrds <isaac.brigido@gmail.com>
2022-07-20 19:09:18 -0300

    Python full - Aula 44 projeto pratico Controller de categoria

--- projetos/pratico01/Controller.py
+++ projetos/pratico01/Controller.py
@@ -34,6 +34,21 @@ class ControllerCategoria:
           arquivo.writelines(i.categoria)
           arquivo.writelines('\n')
 
-categoria = ControllerCategoria()
-categoria.remover('Frios')
+  def alterar(self,categoriaAlterar, categoriaAlterada):
+    categorias = DaoCategoria.ler()
+    categoria_a = list(filter(lambda x: x.categoria == categoriaAlterar, categorias))
+
+    if len(categoria_a) > 0:
+      categoria_nao_existe = list(filter(lambda x: x.categoria == categoriaAlterada, categorias))
+      if len(categoria_nao_existe) == 0:
+        categorias = list(map(lambda x: Categoria(categoriaAlterada) if(x.categoria == categoriaAlterar) else(x), categorias))
+      else:
+        print('A categoria que deseja alterar já existe')
+    else: 
+      print('A categoria que deseja alterar não existe!')
+    
+    with open('categoria.txt', 'w') as arquivo:
+      for i in categorias:
+        arquivo.writelines(i.categoria)
+        arquivo.writelines('\n')
 
