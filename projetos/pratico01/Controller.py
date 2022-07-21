846856ba5c6ee5b1dbbc7ffdc8d23bc817d4cb4f
isaacbrds <isaac.brigido@gmail.com>
2022-07-21 17:32:14 -0300

    Python full - Aula 46 projeto pratico Controller de estoque

--- projetos/pratico01/Controller.py
+++ projetos/pratico01/Controller.py
@@ -28,7 +28,7 @@ class ControllerCategoria:
           break
       
       print('Categoria removida com sucesso!')
-
+      #TODO: remover categoria no estoque
       with open('categoria.txt', 'w') as arquivo:
         for i in categorias:
           arquivo.writelines(i.categoria)
@@ -42,6 +42,7 @@ class ControllerCategoria:
       categoria_nao_existe = list(filter(lambda x: x.categoria == categoriaAlterada, categorias))
       if len(categoria_nao_existe) == 0:
         categorias = list(map(lambda x: Categoria(categoriaAlterada) if(x.categoria == categoriaAlterar) else(x), categorias))
+        #TODO: alterar categoria no estoque
         print('A alteração foi realizada com sucesso!')
       else:
         print('A categoria que deseja alterar já existe')
@@ -64,6 +65,41 @@ class ControllerCategoria:
     else:
       print('Categoria não encontrada!')
 
+class ControllerEstoque:
+  def cadastrar(self, nome, preco, categoria, quantidade):
+    estoque_lido = DaoEstoque.ler()
+    categoria_lida = DaoCategoria.ler()
 
-cat = ControllerCategoria()
-cat.mostrar('Banana')
+    categoria_filtrada = list(filter(lambda x: x.categoria == categoria, categoria_lida))
+    estoque = list(filter(lambda x: x.produto.nome == nome, estoque_lido))
+
+    if len(categoria_filtrada) > 0:
+      if len(estoque) == 0:
+        produto = Produtos(nome, preco, categoria)
+        DaoEstoque.salvar(produto, quantidade)
+        print('Produto cadastrado com sucesso!')
+      else:
+        print('Produto já existe em estoque!')
+    else:
+        print('Categoria inexistente!')
+
+  def remover(self, nome):
+    estoque_lido = DaoEstoque.ler()
+    estoque_filtrado = list(filter(lambda x: x.produto.nome == nome, estoque_lido))
+    if len(estoque_filtrado) > 0:
+      for i in range(estoque_lido):
+        if estoque_lido[i].produto.nome == nome:
+          del(estoque_lido[i])
+          break
+    else:
+      print('Esse produto não existe em estoque')
+    
+    with open('estoque.txt', 'w') as arquivo:
+      for i in estoque_lido:
+        arquivo.writelines(i.produto.nome + "|" + i.produto.preco 
+        + "|" + i.produto.categoria + "|" + str(i.quantidade))
+        arquivo.writelines("\n")
+    
+
+estoque = ControllerEstoque()
+estoque.cadastrar('Banana', '10', 'Frutas', 10)
